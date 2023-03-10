eac2f21746cc6d1045a5878a753bae886647a4dc
ugur <u9urturk@gmail.com>
2023-03-10 17:32:52 +0300

    import loading component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import './App.css';
-import Loading from './pages/Loading';
 import Navbar from './pages/Navbar';
 import Home from './pages/Home';
+import Loading from './components/Loading';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
+  const [isLoading, setisLoading] = useState(false)
+  const test= ()=>{
+    console.log(isLoading)
+
+    setTimeout(() => {
+      setisLoading(true)
+
+      console.log(isLoading)
+    }, 3000);
+  }
+
+  useEffect(() => {
+    test()
+
+  }, [])
+  
+  if(isLoading === true) {
+    return (
     <div className=''> 
         <Navbar></Navbar>
         <Home></Home>
-        <Loading></Loading>
+        
     </div>
-  );
+  )}else{
+
+    return(
+      <div className=''>
+        <Loading></Loading>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from 'react'
+import { GiVacuumCleaner } from "react-icons/gi";
+
+
+export default function Loading() {
+  const ref = useRef()
+
+  useEffect(() => {
+    let result = ref.current.querySelectorAll("#icon")
+     const test=()=>{
+      result[0].classList.add('scale-150')
+      setTimeout(() => {
+        result[0].classList.remove('scale-150')
+      }, 3000);
+     }
+     const test2 = () =>{
+      setTimeout(() => {
+        result[0].classList.remove('scale-150')
+      }, 1000);
+     }
+     test()
+     test2()
+     let interval =setInterval(test,2000)
+     return()=>{
+      clearInterval(interval)
+     }
+   
+}, [ref])
+
+
+  return (
+    <div ref={ref} className="fixed flex items-center  bg-gradient-to-b  from-lime-600   to-lime-100 justify-center top-0 left-0 h-full w-full  backdrop-blur-sm text-orange-400 ">
+      
+        <GiVacuumCleaner id='icon'  className='transition-all' size={150}></GiVacuumCleaner>
+      
+    </div>
+  )
+} 
+ 
